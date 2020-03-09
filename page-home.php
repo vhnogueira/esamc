fcce6ae44c1465ac0134828579ec1fdd3faf894b
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-09 17:03:46 -0300

    CSS changes and home page

---
+++ page-home.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+// while ( have_posts() ) :
+// 	the_post();
+// 	get_template_part( 'loop-templates/content', 'empty' );
+// endwhile;
+
+
+
+get_footer();
