0650490f5487a0fb83856288d16b2568f3a914c6
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-09 21:09:30 -0300

    added first home section

--- page-home.php
+++ page-home.php
@@ -73,6 +73,71 @@ get_header();
       </div>
     </div>
 
+    <div class="bg-clear py-5">
+    <div class="container">
+      <p class="h1 text-center mb-5 font-weight-bold playfair text-secondary">Vídeos</p>
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="card category-graduacao shadow border-0 mb-4 d-flex">
+            <img class="card-img rounded" src="https://placeimg.com/800/400/tech" alt="Card image">
+            <div class="card-img-overlay rounded d-flex">
+              <div class="mt-auto">
+                <div class="video-play" href="//www.youtube.com/watch?v=XSGBVzeBUbk" data-lity></div>
+
+                <div class="mb-2"><span class="py-1 px-2 tag rounded text-uppercase mb-2 text-white">Graduação</span>
+                </div>
+
+                <div class="p-0 card-title text-light text-left">
+                  <span class="h3 font-weight-bold playfair">
+                    Em dúvida do curso? A gente ajuda!
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="col-lg-4">
+          <div class="card category-tecnologo shadow border-0 mb-3 d-flex">
+            <img class="card-img rounded" src="https://placeimg.com/800/400/tech" alt="Card image">
+            <div class="card-img-overlay py-0 px-3 rounded d-flex">
+              <div class="mt-auto">
+                <div class="video-play" href="//www.youtube.com/watch?v=XSGBVzeBUbk" data-lity></div>
+                  
+
+                <div class="mb-2"><span class="py-1 px-2 tag rounded text-uppercase mb-2 text-white">Tecnólogo</span>
+                </div>
+
+                <div class="p-0 card-title text-light text-left">
+                  <span class="h6 font-weight-bold playfair">Em dúvida do curso? A
+                    gente ajuda!</span></div>
+              </div>
+            </div>
+          </div>
+          <div class="card category-mba shadow border-0 mb-4 d-flex">
+            <img class="card-img rounded" src="https://placeimg.com/800/400/tech" alt="Card image">
+            <div class="card-img-overlay py-0 px-3 rounded d-flex">
+              <div class="mt-auto">
+                <div class="video-play" href="//www.youtube.com/watch?v=XSGBVzeBUbk" data-lity></div>
+
+
+                <div class="mb-2"><span class="py-1 px-2 tag rounded text-uppercase mb-2 text-white">MBA</span></div>
+
+                <div class="p-0 card-title text-light text-left">
+                  <span class="h6 font-weight-bold playfair">Em dúvida do curso? A
+                    gente ajuda!</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="text-center">
+        <a href="/clientes/esamcwp/videos.html" class="btn btn-primary btn-lg">Todos os vídeos</a>
+      </div>
+    </div>
+  </div>
+
     <script>
         window.onscroll = scroll;
 
