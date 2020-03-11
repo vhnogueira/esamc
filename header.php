f511f863baa31fe8ef821c1f56455c290e1ae9f7
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-11 10:49:02 -0300

    fixed header

--- header.php
+++ header.php
@@ -26,11 +26,15 @@ $container = get_theme_mod( 'understrap_container_type' );
 <div class="site" id="page">
 
 	<!-- ******************* The Navbar Area ******************* -->
-	<div id="wrapper-navbar" class="<?php if ( is_front_page() ) :
-                                            echo 'fixed-top';
-                                        else :
-                                            echo 'sticky-top';
-                                        endif; ?>">
+	<div id="wrapper-navbar" class="<?php if ( is_front_page() ) : ?>
+
+							'fixed-top'
+
+						<?php else : ?>
+
+							'sticky-top'
+
+						<?php endif; ?>">
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
