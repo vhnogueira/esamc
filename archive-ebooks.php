83819e4e8334614bd0a7df45ff320608d8459bce
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-04-03 10:55:17 -0300

    changes

--- archive-ebooks.php
+++ archive-ebooks.php
@@ -24,56 +24,31 @@ get_header(); ?>
         ?>
 
         <?php if ( $the_query->have_posts() ) : ?>
+          <div class="card-deck">
           <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-            <?php the_title(); ?>
+            <div class="card border-0 shadow">
+            <img class="img-fluid mb-2 rounded" src="https://placeimg.com/800/400/tech" alt="<?php the_title(); ?>">
+            <div class="card-body">
+              <div class="mb-3 category-tecnologo"><span class="py-1 px-2 tag rounded text-uppercase mb-2 text-white">Tecnólogo</span>
+              </div>
+              <p class="h4 font-weight-bold mb-3"><?php the_title(); ?></p>
+              <p><?php the_field('resumo') ?></p>
+              <a href="<?php the_field('link_do_botao'); ?>" class="btn btn-primary btn-block btn-lg"><?php the_field('texto_do_botao'); ?></a>
+            </div>
+          </div>
+
+            
             <?php the_excerpt(); ?>
 
           <?php endwhile; ?>
+          </div>
           <?php wp_reset_postdata(); ?>
 
         <?php else : ?>
           <p><?php __('Nenhum eBook cadastrado'); ?></p>
         <?php endif; ?>
       </div>
-
-        <div class="col-md-4">
-          <div class="card border-0 shadow">
-            <img class="img-fluid mb-2 rounded" src="https://placeimg.com/800/400/tech" alt="Card image">
-            <div class="card-body">
-              <div class="mb-3 category-tecnologo"><span class="py-1 px-2 tag rounded text-uppercase mb-2 text-white">Tecnólogo</span>
-              </div>
-              <p class="h4 font-weight-bold mb-3">Em dúvida do curso? A gente ajuda!</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis id dolores nesciunt debitis saepe facilis nam tenetur aperiam fugit reprehenderit commodi officia eligendi temporibus quis, ratione earum odit provident? Error!</p>
-              <a href="#" class="btn btn-primary btn-block btn-lg">Baixar agora</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card border-0 shadow">
-            <img class="img-fluid mb-2 rounded" src="https://placeimg.com/800/400/tech" alt="Card image">
-            <div class="card-body">
-              <div class="mb-3 category-mba"><span class="py-1 px-2 tag rounded text-uppercase mb-2 text-white">MBA</span>
-              </div>
-              <p class="h4 font-weight-bold mb-3">Em dúvida do curso? A gente ajuda!</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis id dolores nesciunt debitis saepe facilis nam tenetur aperiam fugit reprehenderit commodi officia eligendi temporibus quis, ratione earum odit provident? Error!</p>
-              <a href="#" class="btn btn-primary btn-block btn-lg">Baixar agora</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card border-0 shadow">
-            <img class="img-fluid mb-2 rounded" src="https://placeimg.com/800/400/tech" alt="Card image">
-            <div class="card-body">
-              <div class="mb-3 category-graduacao"><span class="py-1 px-2 tag rounded text-uppercase mb-2 text-white">Graduação</span>
-              </div>
-              <p class="h4 font-weight-bold mb-3">Em dúvida do curso? A gente ajuda!</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis id dolores nesciunt debitis saepe facilis nam tenetur aperiam fugit reprehenderit commodi officia eligendi temporibus quis, ratione earum odit provident? Error!</p>
-              <a href="#" class="btn btn-primary btn-block btn-lg">Baixar agora</a>
-            </div>
-          </div>
-        </div>
-
 
       </div>
     </div>
