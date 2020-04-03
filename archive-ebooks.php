c1ad4449782efaddc52becb43421ef13679d0a5f
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-04-03 10:57:05 -0300

    changes

--- archive-ebooks.php
+++ archive-ebooks.php
@@ -28,18 +28,16 @@ get_header(); ?>
           <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
             <div class="card border-0 shadow">
-            <img class="img-fluid mb-2 rounded" src="https://placeimg.com/800/400/tech" alt="<?php the_title(); ?>">
-            <div class="card-body">
-              <div class="mb-3 category-tecnologo"><span class="py-1 px-2 tag rounded text-uppercase mb-2 text-white">Tecnólogo</span>
+              <img class="img-fluid mb-2 rounded" src="https://placeimg.com/800/400/tech" alt="<?php the_title(); ?>">
+              <div class="card-body">
+                <div class="mb-3 category-tecnologo"><span class="py-1 px-2 tag rounded text-uppercase mb-2 text-white">Tecnólogo</span>
+                </div>
+                <p class="h4 font-weight-bold mb-3"><?php the_title(); ?></p>
+                <p><?php the_field('resumo') ?></p>
+                <a href="<?php the_field('link_do_botao'); ?>" class="btn btn-primary btn-block btn-lg"><?php the_field('texto_do_botao'); ?></a>
               </div>
-              <p class="h4 font-weight-bold mb-3"><?php the_title(); ?></p>
-              <p><?php the_field('resumo') ?></p>
-              <a href="<?php the_field('link_do_botao'); ?>" class="btn btn-primary btn-block btn-lg"><?php the_field('texto_do_botao'); ?></a>
             </div>
-          </div>
 
-            
-            <?php the_excerpt(); ?>
 
           <?php endwhile; ?>
           </div>
